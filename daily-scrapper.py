eb7940eb3a8a78e0c597e4eac056ec824e7bdd77
Tabitoo <lautarovaldez4@gmail.com>
2022-08-17 17:06:00 -0300

    Start implementions for google spreadsheet API

--- daily-scrapper.py
+++ daily-scrapper.py
@@ -9,7 +9,6 @@ from selenium.webdriver.chrome.options import Options
 #Take environment variables from .env
 load_dotenv()
 
-
 path = os.getenv("DRIVER_PATH")
 
 options = Options()
@@ -19,22 +18,20 @@ service = Service(executable_path=str(path))
 driver = webdriver.Chrome(service=service, options=options)
 nameList = []
 hashList = []
-pages = range(10)
 
-for page in reversed(pages):
 
-    website = f'https://nyaa.si/user/puyero?p={page + 1}'
-    driver.get(website)
+website = 'https://nyaa.si/user/puyero?p=1'
+driver.get(website)
 
-    trList = driver.find_elements(by = "xpath", value='//tbody/tr')
+trList = driver.find_elements(by = "xpath", value='//tbody/tr')
 
-    for line in trList:
+for line in trList:
 
-        title = line.find_element(by="xpath", value='./td[@colspan="2"]/a[not(@class="comments")]')
-        link = line.find_elements(by="xpath", value='./td[@class="text-center"]/a')
+    title = line.find_element(by="xpath", value='./td[@colspan="2"]/a[not(@class="comments")]')
+    link = line.find_elements(by="xpath", value='./td[@class="text-center"]/a')
 
-        nameList.append(title.text)
-        hashList.append(link[1].get_attribute("href")[20:60])
+    nameList.append(title.text)
+    hashList.append(link[1].get_attribute("href")[20:60])
 
 animeData = { "tittle" : nameList, "hash" : hashList}
 
@@ -42,3 +39,7 @@ csv_test = pd.DataFrame(animeData)
 csv_test.to_csv("puyasubs-hash.csv")
 
 driver.quit()
+
+
+
+
